522a6dbd1b97076498dbc7d5195ffa78e40d8653
DESKTOP-N6DF3V7\kys <ysookim@puzzleone.com>
2023-11-07 13:41:52 +0900

    Add GameplayAbilities Module

--- Source/UnrealPortfolio/UnrealPortfolio.Build.cs
+++ Source/UnrealPortfolio/UnrealPortfolio.Build.cs
@@ -8,7 +8,7 @@ public class UnrealPortfolio : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
