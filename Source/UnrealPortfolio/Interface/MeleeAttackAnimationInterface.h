93a0cda4496e001732754cd03c6a4e6c5ff1b0e4
DESKTOP-N6DF3V7\kys <ysookim@puzzleone.com>
2023-11-12 20:38:34 +0900

    rename file

--- Source/UnrealPortfolio/Interface/MeleeAttackAnimationInterface.h
+++ Source/UnrealPortfolio/Interface/MeleeAttackAnimationInterface.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
-#include "AttackAnimationInterface.generated.h"
+#include "MeleeAttackAnimationInterface.generated.h"
 
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
